308d02dfc5f72076d3dcb5882bb0fb2625c6cd1c
Makale3301 <makaleharvey2.0@gmail.com>
2026-01-28 12:51:09 -0500

    Completed

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -13,6 +13,23 @@ public class BasicInput {
         userInt = scnr.nextInt();
 
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
+        System.out.println("Enter integer:\n");
+        int myInt = scnr.nextInt();
+
+        System.out.println("Enter double:\n");
+        double myDouble = scnr.nextDouble();
+
+        System.out.println("Enter character:\n");
+        char myChar = scnr.next().charAt(0);
+
+        scnr.nextLine();
+
+        System.out.println("Enter string:\n");
+        String myString = scnr.nextLine();
+
+        System.out.println(myString + " " + myChar + " " + myDouble + " " + myInt);
+
+        scnr.close();
 
         // TODO (2): Output the four values in reverse
 
